50ae148e70b792db14d746fca6b3ac09ea3625a7
dd69 <dd69@ECE-SDLAB2-07A.uanet.edu>
2019-02-12 13:30:46 -0500

    Name Change to txRxRegisterConfiguration.

---
+++ txRxRegisterConfiguration.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+
+#ifndef txRxRegisterConfiguration
+#define	txRxRegisterConfiguration
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+int txSetup(void);
+#endif
