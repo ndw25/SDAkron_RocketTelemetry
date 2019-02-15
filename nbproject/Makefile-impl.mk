c1439187757791a0a201daab756566163c3d175a
ddalvin <47401877+ddalvin@users.noreply.github.com>
2019-02-15 15:30:17 -0500

    config.h added again

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=spiCode.X
+PROJECTNAME=SDAkron_RocketTelemetry
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=spiCode.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/spiCode.X.production.hex
+CND_ARTIFACT_NAME_default=SDAkron_RocketTelemetry.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SDAkron_RocketTelemetry.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=spicode.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/spicode.x.tar
+CND_PACKAGE_NAME_default=sdakronrockettelemetry.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sdakronrockettelemetry.tar
